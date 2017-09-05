a2cfb8cef914e3811023667dcd3aa761a8378015
Gustavo Figueiredo <gustavo@vaast.com.br>
2017-09-04 22:56:33 -0300

    section 7

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,41 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
-    render() {
-        return (
-            <div>
-                Header
-            </div>
-        );
+  renderContent = () => {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return <a href="/auth/google">Login With Google</a>;
+      default:
+        return <a href="/api/logout">Logout {this.props.auth.googleId}</a>;
     }
+  };
+  render() {
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <Link
+            to={this.props.auth ? '/surveys' : '/'}
+            className="left brand-logo"
+          >
+            Emaily
+          </Link>
+          <ul className="right">
+            <li>{this.renderContent()}</li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
 }
 
-export default Header;
+const mapStateToProps = ({ auth }) => {
+  return {
+    auth
+  };
+};
+
+export default connect(mapStateToProps)(Header);
